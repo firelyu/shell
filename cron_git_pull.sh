567d283cc589fbb83ce7e7a79765affd3b79a28a
Yu Liao <liaoyu17@gmail.com>
2012-11-27 22:29:19 -0500

    Add the script to cron to git pull.

---
+++ cron_git_pull.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+GIT="git"
+REPO_LOC="$HOME/workspace/block_dedup"
+LOG_FILE="$HOME/workspace/cron_git_pull.log"
+
+function log_msg {
+    local msg=$1;
+
+    if [ ! -f $LOG_FILE ]; then
+        touch $LOG_FILE
+    fi
+
+    local timestamp=`date +"%x %X"`
+    echo "$timestamp : $msg" >> $LOG_FILE
+}
+
+
+log_msg "Begin to pull $REPO_LOC"
+cd "$REPO_LOC"
+if [ $? -ne 0 ]; then
+    log_msg "Fail to cd $REPO_LOC"
+    exit 1
+fi
+log_msg "Succeed to cd $REPO_LOC"
+
+$GIT pull >>$LOG_FILE 2>&1
+if [ $? -ne 0 ]; then
+    log_msg "Fail to git pull"
+    exit 1
+fi
+log_msg "Succeed to pull $REPO_LOC"
