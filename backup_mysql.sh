d22933d047934e62078432d2677b20dfa4bb672e
Liao Yu <hyadmin@hywgj001.(none)>
2017-02-23 13:21:12 +0800

    add clean_one_old_db()

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -4,9 +4,9 @@ MYSQLDUMP='/usr/bin/mysqldump'
 GZIP='/bin/gzip'
 
 DB_USER='root'
-DB_PASSWORD='xxxxxx'
+DB_PASSWORD='thangga2015'
 
-BACKUP_DB_LIST='mysql db1 db2'
+BACKUP_DB_LIST='xuexiao qiche mysql demo'
 BACKUP_DB_DIR='/var/mysql_backup'
 BACKUP_DB_SUFFIX='sql.gz'
 
@@ -15,15 +15,25 @@ LOG_FILE="${BACKUP_DB_DIR}/backup_mysql.log"
 function log() {
     tag="$1"
     msg="$2"
-    echo "[${tag}] $msg" >> ${LOG_FILE}
+    echo "$(date +"%F %H:%M:%S") [${tag}] $msg" >> ${LOG_FILE}
+}
+
+function usage() {
+    echo "$0 [backup|clean]"
 }
 
 function backup_one_db() {
     dbName="$1"
     
+    subDir=`date +"%Y%m%d"`
+    subDir="${BACKUP_DB_DIR}/${subDir}"
+    if [ ! -d "${subDir}" ]; then
+        mkdir ${subDir}
+    fi
+    
     timestamp=`date +"%Y%m%d_%H%M%S"`
     backupDbName="${dbName}_${timestamp}.${BACKUP_DB_SUFFIX}"
-    backupDbName="${BACKUP_DB_DIR}/${backupDbName}"
+    backupDbName="${subDir}/${backupDbName}"
 
     cmd="${MYSQLDUMP} --opt --user=${DB_USER} --password=${DB_PASSWORD} --default-character-set=utf8 ${dbName} | ${GZIP} > ${backupDbName}"
     log "INFO" "$cmd"
@@ -39,7 +49,41 @@ function backup_one_db() {
     fi
 }
 
-for db in $BACKUP_DB_LIST; do
-    backup_one_db ${db}
-done
+function rm_one_old_db() {
+    dbName="$1"
+    
+    year=$(date +%Y)
 
+    currentMonth=$(date +%m)
+    lastMonth=$((currentMonth - 1))
+    if [ ${#lastMonth} -eq 1 ]; then
+        lastMonth="0$lastMonth"
+    fi
+    
+    for dir in `find ${BACKUP_DB_DIR} -type d -name "${year}${lastMonth}[0-9][0-9]"`; do
+        cmd="rm -f $dir/${dbName}_*.${BACKUP_DB_SUFFIX}"
+        log "INFO" "$cmd"       
+        rm -f $dir/${dbName}_*.${BACKUP_DB_SUFFIX}
+        if [ "$(ls -A $dir)" = "" ]; then
+            cmd="rm -rf $dir"
+            log "INFO" "$cmd"
+            rm -rf $dir
+        fi
+    done
+
+}
+
+# main
+operation=$1
+
+if [ "${operation}" = "backup" ]; then
+    for db in $BACKUP_DB_LIST; do
+        backup_one_db ${db}
+    done
+elif [ "${operation}" = "clean" ]; then
+    for db in $BACKUP_DB_LIST; do
+        rm_one_old_db ${db}
+    done
+else
+    usage
+fi
